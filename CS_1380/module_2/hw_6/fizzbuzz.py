c37794ce95ad0dcd9717fb33db39a428ea64b472
Kjdewitt23 <kollin.dewitt@gmail.com>
2026-09-09 12:45:51 -0600

    cs 1380 hw 6

---
+++ CS_1380/module_2/hw_6/fizzbuzz.py
@@ -0,0 +1,18 @@
+import sys
+
+def fizz_buzz(limit):
+    for i in range(1, limit + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
+
+def main():
+    fizz_buzz(int(sys.argv[1]))
+    
+if __name__ == '__main__':
+    main()
